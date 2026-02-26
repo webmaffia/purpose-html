86adeea37b67df2a037a70ef5388678aad3b9ef9
Javed <javed@marketinsider.in>
2026-02-27 03:56:28 +0530

    final push

--- wall-of-hope.php
+++ wall-of-hope.php
@@ -21,102 +21,223 @@
 
         <!-- Mosaic grid: 12 items, 3 columns (order: col1=1,4,7,10 col2=2,5,8,11 col3=3,6,9,12) -->
         <section class="hopes__mosaic">
+            <h1 class="hopes__mosaic-title">Wall Of Hope</h1>
             <div class="hopes__mosaic-grid">
-                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-left">
-                    <img src="assets/images/wall-of-hopes/1.png" alt="" loading="lazy" />
-                    <div class="hopes__mosaic-content">
-                    <h3>Mugdhakumari Patel</h3>
-                    <p>PEK BD STAT – CRO</p>
-                    <span>India</span>
+                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-left hopes__mosaic-item--bg-1">
+                    <div class="hopes__mosaic-flip">
+                        <div class="hopes__mosaic-front">
+                            <img src="assets/images/wall-of-hopes/1.png" alt="" loading="lazy" />
+                            <div class="hopes__mosaic-content">
+                                <h3>Mugdhakumari Patel</h3>
+                                <p>PEK BD STAT – CRO</p>
+                                <span>India</span>
+                            </div>
+                        </div>
+                        <div class="hopes__mosaic-back">
+                            <div class="hopes__mosaic-back-inner">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <img class="hopes__mosaic-petals" src="assets/images/wall-of-hopes/petals-flip.png" alt="" />
+                        </div>
+                    </div>
                 </div>
+                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-left hopes__mosaic-item--bg-4">
+                    <div class="hopes__mosaic-flip">
+                        <div class="hopes__mosaic-front">
+                            <img src="assets/images/wall-of-hopes/4.png" alt="" loading="lazy" />
+                            <div class="hopes__mosaic-content">
+                                <h3>Rajiv Puri</h3>
+                                <p>API</p>
+                                <span>India</span>
+                            </div>
+                        </div>
+                        <div class="hopes__mosaic-back">
+                            <div class="hopes__mosaic-back-inner">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <img class="hopes__mosaic-petals" src="assets/images/wall-of-hopes/petals-flip.png" alt="" />
+                        </div>
+                    </div>
                 </div>
-                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-left">
-                    <img src="assets/images/wall-of-hopes/4.png" alt="" loading="lazy" />
-                    <div class="hopes__mosaic-content">
-                    <h3>Rajiv Puri</h3>
-                    <p>API</p>
-                    <span>India</span>
+                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-left hopes__mosaic-item--bg-7">
+                    <div class="hopes__mosaic-flip">
+                        <div class="hopes__mosaic-front">
+                            <img src="assets/images/wall-of-hopes/7.png" alt="" loading="lazy" />
+                            <div class="hopes__mosaic-content">
+                                <h3>Amarnath Prakash</h3>
+                                <p>Manufacturing</p>
+                                <span>India</span>
+                            </div>
+                        </div>
+                        <div class="hopes__mosaic-back">
+                            <div class="hopes__mosaic-back-inner">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <img class="hopes__mosaic-petals" src="assets/images/wall-of-hopes/petals-flip.png" alt="" />
+                        </div>
+                    </div>
                 </div>
+                <div class="hopes__mosaic-item hopes__mosaic-item--top-left hopes__mosaic-item--bg-10">
+                    <div class="hopes__mosaic-flip">
+                        <div class="hopes__mosaic-front">
+                            <img src="assets/images/wall-of-hopes/10.png" alt="" loading="lazy" />
+                            <div class="hopes__mosaic-content">
+                                <h3>Kumaran Govindasamy</h3>
+                                <p>IPMG</p>
+                                <span>India</span>
+                            </div>
+                        </div>
+                        <div class="hopes__mosaic-back">
+                            <div class="hopes__mosaic-back-inner">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <img class="hopes__mosaic-petals" src="assets/images/wall-of-hopes/petals-flip.png" alt="" />
+                        </div>
+                    </div>
                 </div>
-                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-left">
-                    <img src="assets/images/wall-of-hopes/7.png" alt="" loading="lazy" />
-                    <div class="hopes__mosaic-content">
-                    <h3>Amarnath Prakash</h3>
-                    <p>Manufacturing</p>
-                    <span>India</span>
+                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-right hopes__mosaic-item--bg-2">
+                    <div class="hopes__mosaic-flip">
+                        <div class="hopes__mosaic-front">
+                            <img src="assets/images/wall-of-hopes/2.png" alt="" loading="lazy" />
+                            <div class="hopes__mosaic-content">
+                                <h3>Awadhkishor Bhadoriya</h3>
+                                <p>IT Operations</p>
+                                <span>India</span>
+                            </div>
+                        </div>
+                        <div class="hopes__mosaic-back">
+                            <div class="hopes__mosaic-back-inner">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <img class="hopes__mosaic-petals" src="assets/images/wall-of-hopes/petals-flip.png" alt="" />
+                        </div>
+                    </div>
                 </div>
+                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-left hopes__mosaic-item--bg-5">
+                    <div class="hopes__mosaic-flip">
+                        <div class="hopes__mosaic-front">
+                            <img src="assets/images/wall-of-hopes/5.png" alt="" loading="lazy" />
+                            <div class="hopes__mosaic-content">
+                                <h3>Nirav Bhatt</h3>
+                                <p>Technical Training</p>
+                                <span>India</span>
+                            </div>
+                        </div>
+                        <div class="hopes__mosaic-back">
+                            <div class="hopes__mosaic-back-inner">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <img class="hopes__mosaic-petals" src="assets/images/wall-of-hopes/petals-flip.png" alt="" />
+                        </div>
+                    </div>
                 </div>
-                <div class="hopes__mosaic-item hopes__mosaic-item--top-left">
-                    <img src="assets/images/wall-of-hopes/10.png" alt="" loading="lazy" />
-                    <div class="hopes__mosaic-content">
-                    <h3>Kumaran Govindasamy</h3>
-                    <p>IPMG</p>
-                    <span>India</span>
+                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-right hopes__mosaic-item--bg-8">
+                    <div class="hopes__mosaic-flip">
+                        <div class="hopes__mosaic-front">
+                            <img src="assets/images/wall-of-hopes/8.png" alt="" loading="lazy" />
+                            <div class="hopes__mosaic-content">
+                                <h3>Prasath S</h3>
+                                <p>Field Force</p>
+                                <span>India</span>
+                            </div>
+                        </div>
+                        <div class="hopes__mosaic-back">
+                            <div class="hopes__mosaic-back-inner">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <img class="hopes__mosaic-petals" src="assets/images/wall-of-hopes/petals-flip.png" alt="" />
+                        </div>
+                    </div>
                 </div>
+                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-right hopes__mosaic-item--bg-11">
+                    <div class="hopes__mosaic-flip">
+                        <div class="hopes__mosaic-front">
+                            <img src="assets/images/wall-of-hopes/11.png" alt="" loading="lazy" />
+                            <div class="hopes__mosaic-content">
+                                <h3>Sarita Dagur</h3>
+                                <p>Mammalian – Upstream Development Team</p>
+                                <span>India</span>
+                            </div>
+                        </div>
+                        <div class="hopes__mosaic-back">
+                            <div class="hopes__mosaic-back-inner">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <img class="hopes__mosaic-petals" src="assets/images/wall-of-hopes/petals-flip.png" alt="" />
+                        </div>
+                    </div>
                 </div>
-                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-right">
-                    <img src="assets/images/wall-of-hopes/2.png" alt="" loading="lazy" />
-                    <div class="hopes__mosaic-content">
-                    <h3>Awadhkishor Bhadoriya</h3>
-                    <p>IT Operations</p>
-                    <span>India</span>
+                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-right hopes__mosaic-item--bg-3">
+                    <div class="hopes__mosaic-flip">
+                        <div class="hopes__mosaic-front">
+                            <img src="assets/images/wall-of-hopes/3.png" alt="" loading="lazy" />
+                            <div class="hopes__mosaic-content">
+                                <h3>Fabiana Goncalves</h3>
+                                <p>Quality Control</p>
+                                <span>Brazil</span>
+                            </div>
+                        </div>
+                        <div class="hopes__mosaic-back">
+                            <div class="hopes__mosaic-back-inner">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <img class="hopes__mosaic-petals" src="assets/images/wall-of-hopes/petals-flip.png" alt="" />
+                        </div>
+                    </div>
                 </div>
+                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-right hopes__mosaic-item--bg-6">
+                    <div class="hopes__mosaic-flip">
+                        <div class="hopes__mosaic-front">
+                            <img src="assets/images/wall-of-hopes/6.png" alt="" loading="lazy" />
+                            <div class="hopes__mosaic-content">
+                                <h3>Meghna Dixit</h3>
+                                <p>Mammalian – Upstream Development Team</p>
+                                <span>India</span>
+                            </div>
+                        </div>
+                        <div class="hopes__mosaic-back">
+                            <div class="hopes__mosaic-back-inner">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <img class="hopes__mosaic-petals" src="assets/images/wall-of-hopes/petals-flip.png" alt="" />
+                        </div>
+                    </div>
                 </div>
-                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-left">
-                    <img src="assets/images/wall-of-hopes/5.png" alt="" loading="lazy" />
-                    <div class="hopes__mosaic-content">
-                    <h3>Nirav Bhatt</h3>
-                    <p>Technical Training</p>
-                    <span>India</span>
+                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-right hopes__mosaic-item--bg-9">
+                    <div class="hopes__mosaic-flip">
+                        <div class="hopes__mosaic-front">
+                            <img src="assets/images/wall-of-hopes/9.png" alt="" loading="lazy" />
+                            <div class="hopes__mosaic-content">
+                                <h3>Ledisilane Assis</h3>
+                                <p>HR</p>
+                                <span>Brazil</span>
+                            </div>
+                        </div>
+                        <div class="hopes__mosaic-back">
+                            <div class="hopes__mosaic-back-inner">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <img class="hopes__mosaic-petals" src="assets/images/wall-of-hopes/petals-flip.png" alt="" />
+                        </div>
+                    </div>
                 </div>
-                </div>
-                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-right">
-                    <img src="assets/images/wall-of-hopes/8.png" alt="" loading="lazy" />
-                    <div class="hopes__mosaic-content">
-                    <h3>Prasath S</h3>
-                    <p>Field Force</p>
-                    <span>India</span>
-                </div>
-                </div>
-                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-right">
-                    <img src="assets/images/wall-of-hopes/11.png" alt="" loading="lazy" />
-                    <div class="hopes__mosaic-content">
-                    <h3>Sarita Dagur</h3>
-                    <p>Mammalian – Upstream Development Team</p>
-                    <span>India</span>
-                </div>
-                </div>
-                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-right">
-                    <img src="assets/images/wall-of-hopes/3.png" alt="" loading="lazy" />
-                    <div class="hopes__mosaic-content">
-                    <h3>Fabiana Goncalves</h3>
-                    <p>Quality Control</p>
-                    <span>Brazil</span>
-                </div>
-                </div>
-                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-right">
-                    <img src="assets/images/wall-of-hopes/6.png" alt="" loading="lazy" />
-                    <div class="hopes__mosaic-content">
-                    <h3>Meghna Dixit</h3>
-                    <p>Mammalian – Upstream Development Team</p>
-                    <span>India</span>
-                </div>
-                </div>
-                <div class="hopes__mosaic-item hopes__mosaic-item--bottom-right">
-                    <img src="assets/images/wall-of-hopes/9.png" alt="" loading="lazy" />
-                    <div class="hopes__mosaic-content">
-                    <h3>Ledisilane Assis</h3>
-                    <p>HR</p>
-                    <span>Brazil</span>
-                </div>
-                </div>
-                <div class="hopes__mosaic-item hopes__mosaic-item--top-left">
-                    <img src="assets/images/wall-of-hopes/12.png" alt="" loading="lazy" />
-                    <div class="hopes__mosaic-content">
-                    <h3>Kalpesh Parab</h3>
-                    <p>Finance Shared Services</p>
-                    <span>India</span>
-                </div>
+                <div class="hopes__mosaic-item hopes__mosaic-item--top-left hopes__mosaic-item--bg-12">
+                    <div class="hopes__mosaic-flip">
+                        <div class="hopes__mosaic-front">
+                            <img src="assets/images/wall-of-hopes/12.png" alt="" loading="lazy" />
+                            <div class="hopes__mosaic-content">
+                                <h3>Kalpesh Parab</h3>
+                                <p>Finance Shared Services</p>
+                                <span>India</span>
+                            </div>
+                        </div>
+                        <div class="hopes__mosaic-back">
+                            <div class="hopes__mosaic-back-inner">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <img class="hopes__mosaic-petals" src="assets/images/wall-of-hopes/petals-flip.png" alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
