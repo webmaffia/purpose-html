6f279a227a53d0e7e7fc2ac134c6d3c7a057a5e7
Javed <javed.mistari@webmaffia.com>
2026-02-26 13:25:41 +0530

    half completed hopes page

---
+++ wall-of-hope.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once 'head.php'; ?>
+
+    <title></title>
+
+</head>
+
+<body>
+    <!-- Header -->
+    <?php require_once 'header.php'; ?>
+
+    <!-- Main container -->
+    <section>
+        <!-- Hero section -->
+        <section class="hopes__hero">
+            <img class="hopes__hero-image" src="assets/images/hero/wall-of-hopes-hero.png" alt="hopes section image"
+                fetchpriority="high" />
+        </section>
+
+        <!-- Cards grid layout -->
+        <section class="wall">
+            <div>
+                <!-- Title -->
+                <h2 class="wall__title">WALL OF HOPE</h2>
+
+                <div class="wall__grid">
+
+                    <!-- 1 -->
+                    <div class="card card--tall">
+                        <img src="assets/images/wall-of-hopes\Mugdhakumari Patel.jpg" alt="Mugdhakumari Patel">
+                        <div class="card__content">
+                            <h3>Mugdhakumari Patel</h3>
+                            <p>PEK BD STAT – CRO</p>
+                            <span>India</span>
+                        </div>
+                    </div>
+
+                    <!-- 2 -->
+                    <div class="card">
+                        <img src="images/niravbhatt.png" alt="Nirav Bhatt">
+                        <div class="card__content">
+                            <h3>Nirav Bhatt</h3>
+                            <p>Technical Training</p>
+                            <span>India</span>
+                        </div>
+                    </div>
+
+                    <!-- 3 -->
+                    <div class="card card--tall">
+                        <img src="images/kumarangovindasamy.png" alt="Kumaran Govindasamy">
+                        <div class="card__content">
+                            <h3>Kumaran Govindasamy</h3>
+                            <p>IPMG</p>
+                            <span>India</span>
+                        </div>
+                    </div>
+
+                    <!-- 4 -->
+                    <div class="card">
+                        <img src="images/meghnadixit.png" alt="Meghna Dixit">
+                        <div class="card__content">
+                            <h3>Meghna Dixit</h3>
+                            <p>Mammalian – Upstream Development Team</p>
+                            <span>India</span>
+                        </div>
+                    </div>
+
+                    <!-- 5 -->
+                    <div class="card card--wide">
+                        <img src="images/awadhkishor.png" alt="Awadhkishor Bhadoriya">
+                        <div class="card__content">
+                            <h3>Awadhkishor Bhadoriya</h3>
+                            <p>IT Operations</p>
+                            <span>India</span>
+                        </div>
+                    </div>
+
+                    <!-- 6 -->
+                    <div class="card">
+                        <img src="images/amarnathprakash.png" alt="Amarnath Prakash">
+                        <div class="card__content">
+                            <h3>Amarnath Prakash</h3>
+                            <p>Manufacturing</p>
+                            <span>India</span>
+                        </div>
+                    </div>
+
+                    <!-- 7 -->
+                    <div class="card">
+                        <img src="images/saritadagur.png" alt="Sarita Dagur">
+                        <div class="card__content">
+                            <h3>Sarita Dagur</h3>
+                            <p>Mammalian – Upstream Development Team</p>
+                            <span>India</span>
+                        </div>
+                    </div>
+
+                    <!-- 8 -->
+                    <div class="card">
+                        <img src="images/fabiana.png" alt="Fabiana Goncalves">
+                        <div class="card__content">
+                            <h3>Fabiana Goncalves</h3>
+                            <p>Quality Control</p>
+                            <span>Brazil</span>
+                        </div>
+                    </div>
+
+                    <!-- 9 -->
+                    <div class="card">
+                        <img src="images/prasaths.png" alt="Prasath S">
+                        <div class="card__content">
+                            <h3>Prasath S</h3>
+                            <p>Field Force</p>
+                            <span>India</span>
+                        </div>
+                    </div>
+
+                    <!-- 10 -->
+                    <div class="card card--tall">
+                        <img src="images/kalpeshparab.png" alt="Kalpesh Parab">
+                        <div class="card__content">
+                            <h3>Kalpesh Parab</h3>
+                            <p>Finance Shared Services</p>
+                            <span>India</span>
+                        </div>
+                    </div>
+
+                    <!-- 11 -->
+                    <div class="card">
+                        <img src="images/rajivpuri.png" alt="Rajiv Puri">
+                        <div class="card__content">
+                            <h3>Rajiv Puri</h3>
+                            <p>API</p>
+                            <span>India</span>
+                        </div>
+                    </div>
+
+                    <!-- 12 -->
+                    <div class="card card--wide">
+                        <img src="images/ledisilaneassis.png" alt="Ledisilane Assis">
+                        <div class="card__content">
+                            <h3>Ledisilane Assis</h3>
+                            <p>HR</p>
+                            <span>Brazil</span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="wall__footer">
+                    <a href="#" class="wall__button">View All</a>
+                </div>
+
+            </div>
+        </section>
+    </section>
+
+    <!-- Footer -->
+    <?php require_once 'footer.php'; ?>
+
+</body>
+
+</html>
